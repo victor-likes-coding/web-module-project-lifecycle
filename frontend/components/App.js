cd20695f22d7672895842465a2fb648cb14f7215
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-14 21:06:59 -0500

    write constructor and componentDidMount

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-const URL = 'http://localhost:9000/api/todos'
+const URL = "http://localhost:9000/api/todos";
 
 export default class App extends React.Component {
-  render() {
-    return null
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch(URL)
+            .then((response) => response.json())
+            .then((data) => this.setState({ todos: data }));
+    }
+    render() {
+        console.log(this.state.todos);
+        return null;
+    }
 }
