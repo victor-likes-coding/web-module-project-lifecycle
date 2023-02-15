6605957cd1fe0ea8fd150571f45479a624548407
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-14 21:08:49 -0500

    write constructor

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default class TodoList extends React.Component {
-  render() {
-    return null
-  }
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return null;
+    }
 }
