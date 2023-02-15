e70fc447557122bf9a4e6069745e7171c51b2397
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-14 21:17:10 -0500

    add the form

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -9,6 +9,16 @@ export default class Form extends React.Component {
     }
 
     render() {
-        return null;
+        return (
+            <form>
+                <input
+                    type="text"
+                    value={this.state.name}
+                    onChange={(e) => this.setState({ name: e.target.value })}
+                />
+                <button type="submit">Add Todo</button>
+                <button>{this.props.showCompleted ? "Hide" : "Show"} Completed</button>
+            </form>
+        );
     }
 }
