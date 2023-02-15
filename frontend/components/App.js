213f368caa7f3868560a8a7df7120e55cab7a5bd
Victor Tran <victor-likes-coding@tempestas.io>
2023-02-14 21:13:57 -0500

    destructure data prop

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
     componentDidMount() {
         fetch(URL)
             .then((response) => response.json())
-            .then((data) => this.setState({ todos: data }));
+            .then(({ data }) => this.setState({ todos: data }));
     }
     render() {
         return (
